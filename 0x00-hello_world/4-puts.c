73f08eec1ee2ee752f243d0598de68ed2d71875c
Ememusoh <100931334+Ememusoh@users.noreply.github.com>
2022-09-08 20:04:11 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/*
- * main:prints out a string via puts and marks entry point 
- * Description: Program that prints out the string s followed by a new line
- * Return: int 0 when done and no errors
+#include<stdio.h>
+/**
+ * main- entry point for program
+ * 
+ * Reurn:int 0 when done and n errors
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
