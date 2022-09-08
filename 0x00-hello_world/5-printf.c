a2eca9f4c1a065092ede8521b0e6cb38bc5f1e3d
Ememusoh <ejemenusoh@gmail.com>
2022-09-08 07:49:46 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - prints out a string with printf
+ * Description: Program that prints out the string s followed by a new line
+ * Return: int 0 when done and no errors
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
