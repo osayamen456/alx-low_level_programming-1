8285ef651d945ea681000a7fc15a2aa1b919aa47
Ememusoh <ejemenusoh@gmail.com>
2022-09-08 11:33:53 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/**
- * main - prints out a string via puts and marks entry point 
+/*
+ * main:prints out a string via puts and marks entry point 
  * Description: Program that prints out the string s followed by a new line
  * Return: int 0 when done and no errors
  */
