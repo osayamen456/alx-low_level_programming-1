4cd02f27a2dd2327059b77f0566ad4743dafd8e6
Ememusoh <ejemenusoh@gmail.com>
2022-09-08 11:57:30 -0700

    puts.c

--- 0x00-hello_world/4.puts.c
+++ 0x00-hello_world/4.puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main- entry point for program
- * Description:program that prints out a string with a newline
+ * 
  * Reurn:int 0 when done and n errors
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
