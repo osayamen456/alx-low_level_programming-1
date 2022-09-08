b0ce54b94d92027b640c4ded2abab97dc33fe922
Ememusoh <ejemenusoh@gmail.com>
2022-09-08 07:35:39 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/**
- * * main - prints out a string via puts and marks entry point 
- * * Description: Program that prints out the string s followed by a new line
- * * Return: int 0 when done and no errors
- * */
+/** 
+* main - entry point
+*
+* Return: 0 after printing the fuction
+*/ 
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
