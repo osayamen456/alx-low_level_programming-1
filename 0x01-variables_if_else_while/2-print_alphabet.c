793ef9f110682a33b04477f8eafb0ad7016380b3
Ememusoh <100931334+Ememusoh@users.noreply.github.com>
2022-09-09 15:02:04 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the beginning of the code
+ * Description: This prints out the alphabets
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
